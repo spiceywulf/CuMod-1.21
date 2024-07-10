1fd4c5cd9faf4e188f286b805b4637f67f31f92e
spiceywulf <marber1406@gmail.com>
2024-07-11 00:17:39 +0200

    Test Test

---
+++ src/main/java/net/spiwu/cumod/CuModClient.java
@@ -0,0 +1,10 @@
+package net.spiwu.cumod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CuModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
